5c779e5252c56cc15296ecd3fbb239c7bc0de77e
sk1zm0509 <jayantaj0509@gmail.com>
2024-02-29 08:48:34 -0500

    Pushing all the codes

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+
+require('./models/User');
+require('./services/passport');
+
+
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({Were :"up and running"});
-});
+
+require('./router/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+    
